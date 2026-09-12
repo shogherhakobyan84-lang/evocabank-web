acaeafa683a8b8d6c50988d069adb7f630540459
Shogher Hakobyan <shogherhakobyan@MacBook-Air-Shogher.local>
2026-09-12 13:34:26 +0400

    remove LiveChat from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -95,72 +95,48 @@ import EvocaTouch from './pages/EvocaTouch';
 import EvocaOnline from './pages/EvocaOnline';
 import Business from './pages/Business';
 import InstantPayments from './components/InstantPayments';
-import LiveChat from './components/LiveChat'; 
+// Եթե այլևս App.jsx-ում չես օգտագործում LiveChat-ը, կարող ես իմպորտելն էլ ջնջել այստեղից
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-       
         <Route path="/" element={<MainLayout />}>
-         
           <Route index element={<Home />} />
-          
-          
           <Route path="business" element={<Business />} />
           <Route path="hy/business" element={<Business />} />
-
-          
           <Route path="hy/loans" element={<Loans />} />
           <Route path="loans" element={<Loans />} />
-
-      
           <Route path="cards" element={<Cards />} />
           <Route path="hy/cards" element={<Cards />} />
-
-         
           <Route path="deposits" element={<Deposits />} />
           <Route path="hy/deposits" element={<Deposits />} />
-
-         
           <Route path="accounts" element={<Accounts />} />
           <Route path="hy/accounts" element={<Accounts />} />
-
-      
           <Route path="transfers" element={<Transfers />} />
           <Route path="hy/transfers" element={<Transfers />} />
-
-      
           <Route path="securities" element={<Securities />} />
           <Route path="hy/securities" element={<Securities />} />
-
-        
           <Route path="evoca-salary" element={<EvocaSalary />} />
           <Route path="hy/evoca-salary" element={<EvocaSalary />} />
           <Route path="evocasalary" element={<EvocaSalary />} />
           <Route path="hy/evocasalary" element={<EvocaSalary />} />
-
-       
           <Route path="evoca-touch" element={<EvocaTouch />} />
           <Route path="hy/evoca-touch" element={<EvocaTouch />} />
           <Route path="evocatouch" element={<EvocaTouch />} />
           <Route path="hy/evocatouch" element={<EvocaTouch />} />
         </Route>
 
-     
         <Route path="evoca-online" element={<EvocaOnline />} />
         <Route path="hy/evoca-online" element={<EvocaOnline />} />
         <Route path="evocaonline" element={<EvocaOnline />} />
         <Route path="hy/evocaonline" element={<EvocaOnline />} />
 
-        {/* Ակնթարթային վճարումների էջի երթուղին (բացվում է առանձին թաբում) */}
         <Route path="instant-payments" element={<InstantPayments />} />
         <Route path="hy/instant-payments" element={<InstantPayments />} />
-
       </Routes>
 
-   
-      <LiveChat />
+      {/* Այստեղից <LiveChat />-ը հեռացված է, հետևաբար այն կանհետանա այն էջերից, որտեղ հարկավոր չէ */}
     </BrowserRouter>
   );
 }
